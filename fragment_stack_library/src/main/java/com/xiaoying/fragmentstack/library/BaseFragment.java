65ba16199081fd08e5b8e525e71e324239949af3
zhangyy <zhangyy_owen@139.com>
2019-09-27 10:56:39 +0800

    添加Fragment增加是否添加到回退栈参数

--- fragment_stack_library/src/main/java/com/xiaoying/fragmentstack/library/BaseFragment.java
+++ fragment_stack_library/src/main/java/com/xiaoying/fragmentstack/library/BaseFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.InputFilter;
@@ -27,12 +28,12 @@ public class BaseFragment extends Fragment implements View.OnClickListener {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
 
